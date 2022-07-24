421c6919e70798be22a83556493cfd38cb19cd73
Pascal Steffen <pascalsteffenbusiness@hotmail.com>
2022-07-24 11:22:10 +0200

    update 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,115 @@
 let currentPokemon;
 
+
 async function init() {
-    await renderCharizard();
+    await renderPokemon();
 
 }
+
 
 /**
  * Render Charmander Pokemon
  */
-async function renderCharizard() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charizard';
-    let response = await fetch(url);
-    let currentPokemon = await response.json();
-    let types = currentPokemon['types'];
-    let charizard = document.getElementById("charizard");
-    let charizardClass = document.getElementById("currentPokemonClass");
+async function renderPokemon() {
+    for (let i = 1; i < 20; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        let currentPokemon = await response.json();
+        let types = currentPokemon['types'];
+        let pokemons = document.getElementById('pokemons');
+        let pokedexId = currentPokemon['id'];
+        pokemons.innerHTML += generateCurrentPokemonContainer(currentPokemon, i, pokedexId);
 
-    charizard.innerHTML = generateCurrentPokemonContainer(currentPokemon);
+        for (let j = 0; j < types.length; j++) {
+            let pokemonClass = document.getElementById(`pokemonClass${i}`);
+            pokemonClass.innerHTML += generateCurrentPokemonHTMLClass(currentPokemon, j);
 
-    for (let i = 0; i < types.length; i++) {
+            pokemonColorChange(currentPokemon, i);
+        }
 
-        charizardClass.innerHTML += generateCurrentPokemonHTMLClass(currentPokemon, i);
+        if (pokedexId > 0 && pokedexId < 10) {
+            document.getElementById(`newPokedexId${i}`).innerHTML = `0${pokedexId}`;
+        }
     }
-
 
 }
 
 
-function generateCurrentPokemonContainer(currentPokemon) {
+/**
+ * Rendert den Namen und das Bild des Pokemons
+ * @param {*} currentPokemon - JSON aus der "renderChrizard()" function.
+ * @returns 
+ */
+function generateCurrentPokemonContainer(currentPokemon, i, pokedexId) {
     let newName = currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1);
+
+
     return /*html*/ `
-    <div class="target-container flex-center mx-4 my-4 px-4 py-4">
+    <div id="classColor${i}" class="target-container flex-start mx-5 my-4 px-4 py-4">
         <div class="flex-column-start">
             <h1><b>${newName}</b></h1>
-            <div id="currentPokemonClass"></div>
+            <span class="pokedex-id" id="newPokedexId${i}">${pokedexId}</span>
+            <div id="pokemonClass${i}"></div>
         </div>
         <div>
             <img class="icon-inline img-100" src="img/pokeball_icon.png" alt="">
         </div>
         <div>
-            <img class="img-150 pokemon-img mx-2" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" alt="">
+            <img class="img-150 pokemon-img" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" alt="">
         </div>
     </div>`
 }
 
-function generateCurrentPokemonHTMLClass(currentPokemon, i) {
-    let newClass = currentPokemon['types'][i]['type']['name'].charAt(0).toUpperCase() + currentPokemon['types'][i]['type']['name'].slice(1);
-    return /*html*/ `
-        <div>
-            <span class="target-container-child px-3 py-1 my-2">${newClass}</span>
-        </div>
 
-    `
+/**
+ * Rendert die Klassen des Pokemons
+ * @param {*} currentPokemon -JSON aus der "renderChrizard()" function.
+ * @param {*} i - Holt sich die Anzahl der Klassen des jeweiligen Pokemons.
+ * @returns 
+ */
+function generateCurrentPokemonHTMLClass(currentPokemon, j) {
+    let newClass = currentPokemon['types'][j]['type']['name'].charAt(0).toUpperCase() + currentPokemon['types'][j]['type']['name'].slice(1);
+    return /*html*/ `
+        <div class="my-2 flex-start">
+            <span class="target-container-child px-3 py-1">${newClass}</span>
+        </div>`
+}
+
+
+/**
+ * Farbdarstellung aller Klassen.
+ * @param {*} currentPokemon - JSON aus der "renderChrizard()" function.
+ * @param {*} i - Holt die Mainfarbe des jeweiligen Pokemons aus dem Array.
+ */
+function pokemonColorChange(currentPokemon, i) {
+    let classColor = document.getElementById(`classColor${i}`);
+    if (currentPokemon['types'][0]['type']['name'] == 'fire') {
+        classColor.classList.add("fire-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'grass' || currentPokemon['types'][0]['type']['name'] == 'bug') {
+        classColor.classList.add("grass-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'poison') {
+        classColor.classList.add("poisen-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'water') {
+        classColor.classList.add("water-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'normal') {
+        classColor.classList.add("normal-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'electric') {
+        classColor.classList.add("electric-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'ground') {
+        classColor.classList.add("ground-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'fairy') {
+        classColor.classList.add("fairy-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'fighting') {
+        classColor.classList.add("fighting-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'ghost') {
+        classColor.classList.add("ghost-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'rock') {
+        classColor.classList.add("rock-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'psychic') {
+        classColor.classList.add("psychic-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'dragon') {
+        classColor.classList.add("dragon-class");
+    } else if (currentPokemon['types'][0]['type']['name'] == 'ice') {
+        classColor.classList.add("ice-class");
+    }
 }
